96032820d2219ef137a8f850f6e1d01ab7a77848
Isaac Zidany <zidanyisaac@gmail.com>
2026-01-13 02:39:49 -0300

    transferido calculos de delta e raizes para funções próprias

--- calcbasc.go
+++ calcbasc.go
@@ -1,9 +1,22 @@
 //simples calculadora para raízes de equações do segundo grau.
 package main
 import ("fmt"; "math")
+func calcularDelta (a float64, b float64, c float64) float64 {
+  delta := b*b - 4*a*c
+	return delta
+}
+func calcularRaizes ( a float64, b float64, delta float64) (float64, float64) {
+	var x1, x2 float64
+	x1 = (-b + math.Sqrt(delta))/(2 * a)
+	x2 = (-b - math.Sqrt(delta))/(2 * a)
+
+	return x1, x2
+}
+
 func main() {
 //variáveis
 	var a, b, c float64
+
 //coletar input do usuário
 	fmt.Println(`
 	==================================================
@@ -12,19 +25,18 @@ func main() {
 	`)
 	fmt.Scan(&a,&b,&c)
 //calculo do delta
-	delta := b*b - 4*a*c
-//diferenciar propriedades do delta
+	delta := calcularDelta (a, b, c)
+//verificação das propriedades e calculo das raizes
 	switch {
 	 case delta == 0:
-		x1 := (-b + math.Sqrt(delta)) / (2*a)
+		var x1, _ = calcularRaizes (a, b, delta)
 		fmt.Println(`
 		Sua equação possui uma única raiz real.
 		Única Raiz:`, x1)
 	 case delta < 0:
 		fmt.Println("Sua equação não possui raízes reais")
 	 default:
-		x1 := (-b + math.Sqrt(delta)) / (2*a)
-		x2 := (-b - math.Sqrt(delta)) / (2*a)
+		x1, x2 := calcularRaizes(a, b, delta)
 		fmt.Println(`
 		Sua equação possui duas raízes reais
 		Primeira raiz:`, x1,
